d35c886aeb8e715a683281f3f39b3c8f3c575daa
Yoshiki Masuyama <mas03151997@gmail.com>
2020-05-08 11:29:14 +0900

    Add propoesed2

--- src/utils/phase_reconstruction.py
+++ src/utils/phase_reconstruction.py
@@ -13,6 +13,13 @@ def projection_consistencysum(x1, x2, mixture, stft, istft):
     y2 = stft(f2+d/2)
     return y1, y2
 
+def projection_sum(f1, f2, mixture):
+
+    d  = mixture-(f1+f2)
+    f1 += d/2
+    f2 += d/2
+    return f1, f2
+
 def prox_dis(x, amp, gamma):
     y = mysign(x)*(np.abs(x)-gamma/(amp)+np.sqrt((np.abs(x)-gamma/(amp))**2+4*gamma))/2;
     return y
@@ -68,4 +75,37 @@ def divmisi_ver1(spec1, spec2, mixture, stft, istft, maxiter=50, gamma=0.5, lm=1
     
     f1est = istft(prox_dis(z1, amp1, gamma))
     f2est = istft(prox_dis(z2, amp2, gamma))
+    return f1est, f2est
+
+
+def divmisi_ver2(spec1, spec2, mixture, stft, istft, maxiter=50, gamma=0.5, lm=1e4, epsilon=1e-15):
+
+    z1 = istft(spec1);
+    z2 = istft(spec2);
+    amp1 = np.abs(spec1)+epsilon;
+    amp2 = np.abs(spec2)+epsilon;
+    
+    u1 = np.zeros_like(spec1);
+    u2 = np.zeros_like(spec2);
+    
+    for i in range(maxiter):
+        
+        # x-update
+        x1 = prox_dis(stft(z1)-u1, amp1, gamma)
+        x2 = prox_dis(stft(z2)-u2, amp2, gamma)
+        
+        # z-update
+        v1 = istft(x1+u1);
+        v2 = istft(x2+u2);
+        [y1, y2] = projection_sum(v1, v2, mixture);
+        z1 = (v1+gamma*lm*y1)/(1+gamma*lm);
+        z2 = (v2+gamma*lm*y2)/(1+gamma*lm);
+        
+        # u-update
+        u1 = u1+x1-stft(z1);
+        u2 = u2+x2-stft(z2);
+
+    
+    f1est = istft(prox_dis(x1, amp1, gamma))
+    f2est = istft(prox_dis(x2, amp2, gamma))
     return f1est, f2est
